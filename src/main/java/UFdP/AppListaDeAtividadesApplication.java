90c0fd2e39f32536883d93a1e60e0382cdbc7476
Bowie <joaovitormpadilha@gmail.com>
2024-12-16 16:15:24 -0300

    primeiro commit

---
+++ src/main/java/UFdP/AppListaDeAtividadesApplication.java
@@ -0,0 +1,13 @@
+package UFdP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppListaDeAtividadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppListaDeAtividadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UFdP/init/UfdpDbApplicationTests.java
@@ -0,0 +1,13 @@
+package UFdP.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfdpDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
